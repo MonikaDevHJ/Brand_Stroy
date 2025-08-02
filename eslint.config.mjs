e38bb789b8ec2a04ab427c07b0060263f60b4243
Monika H J <monikahj7@gmail.com>
2025-08-02 23:06:04 +0530

    done

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // 👇 Add this block to disable unescaped entities warning
+  {
+    rules: {
+      "react/no-unescaped-entities": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
