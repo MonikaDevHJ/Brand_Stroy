0023fbd62caeab46f44b116cd6a16c3fc05af0a4
Monika H J <monikahj7@gmail.com>
2025-08-02 18:36:40 +0530

    making

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
 import Image from "next/image";
 import Herosection from "./component/Herosection";
 import Comprehensive from "./component/Comprehensive"
+import DominateDubai from "./component/DominateDubai"
+import  WhyChose from ".//component/WhyChose"
+import OurComprensive from "./component/OurComprensive"
 
 export default function Home() {
   return (
  <>
  <Herosection/>
+  <DominateDubai/>
  <Comprehensive/>
+ <WhyChose/>
+ <OurComprensive/>
  </>
   );
 }
